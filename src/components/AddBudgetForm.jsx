ee5146ce230a8cb9603555d31a00b3ac4db9809c
Lucas Quintanilha <l.quitan@gmail.com>
2023-09-25 23:45:45 -0300

    feat: created Budget component and basic logic

---
+++ src/components/AddBudgetForm.jsx
@@ -0,0 +1,43 @@
+// rdd imports
+import { Form } from 'react-router-dom';
+
+// library
+import { CurrencyDollarIcon } from '@heroicons/react/24/solid';
+
+const AddBudgetForm = () => {
+  return (
+    <div className="form-wrapper">
+      <h2 className="h3">Create budget</h2>
+      <Form method="post" className="grid-sm">
+        <div className="grid-xs">
+          <label htmlFor="newBudget">Budget Name</label>
+          <input
+            type="text"
+            name="newBudget"
+            id="newBudget"
+            placeholder="e.g., Groceries"
+          />
+        </div>
+        <div className="grid-xs">
+          <label htmlFor="newBudgetAmount">Budget Amount</label>
+          <input
+            type="number"
+            step="0.01"
+            name="newBudgetAmount"
+            id="newBudgetAmount"
+            placeholder="e.g., U$ 100"
+            required
+            inputMode="decimal"
+          />
+        </div>
+        <input type="hidden" name="_action" value="createBudget" />
+        <button type="subimit" className="btn btn--dark">
+          <span>Create Budget</span>
+          <CurrencyDollarIcon width={20} />
+        </button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddBudgetForm;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,31 +3,66 @@ import { useLoaderData } from 'react-router-dom';
 
 // components
 import Intro from '../components/Intro';
+import AddBudgetForm from '../components/AddBudgetForm';
 
 //  helper functions
-import { fetchData } from '../helpers';
+import { createBudget, fetchData } from '../helpers';
 import { toast } from 'react-toastify';
 
 // loader
 export function dashboardLoader() {
   const userName = fetchData('userName');
-  return { userName };
+  const budgets = fetchData('budgets');
+  return { userName, budgets };
 }
 
 // action
 export async function dashboardAction({ request }) {
   const data = await request.formData();
-  const formData = Object.fromEntries(data);
-  try {
-    localStorage.setItem('userName', JSON.stringify(formData.userName));
-    return toast.success(`Wellcome, ${formData.userName}`);
-  } catch (e) {
-    throw new Error('There is a problem with your account.');
+  const { _action, ...values } = Object.fromEntries(data);
+
+  // new user submission
+  if (_action == 'newUser') {
+    try {
+      localStorage.setItem('userName', JSON.stringify(values.userName));
+      return toast.success(`Wellcome, ${values.userName}`);
+    } catch (e) {
+      throw new Error('There is a problem with your account.');
+    }
+  }
+  // new budget
+  if (_action == 'createBudget') {
+    try {
+      createBudget({
+        name: values.newBudget,
+        amount: values.newBudgetAmount,
+      });
+      /** some logic*/
+      return toast.success('Budget created!');
+    } catch (e) {
+      throw new Error('There is a problem with your budget');
+    }
   }
 }
 
 const Dashboard = () => {
   const { userName } = useLoaderData();
-  return <>{userName ? <h1>{userName}</h1> : <Intro />}</>;
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Wellcome back, <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">{/** some logic */}</div>
+          <div className="gird-lg">
+            <AddBudgetForm />
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 };
 export default Dashboard;
